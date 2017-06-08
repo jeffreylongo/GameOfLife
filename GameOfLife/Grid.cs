aab5f0b80300b3b958513972ee4d64796fd3401b
Jeff Longo <jeffreylongo@yahoo.com>
2017-06-08 14:29:20 -0400

    created grid class and createTheGrid method with initial grid seed

---
+++ GameOfLife/Grid.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    class Grid
+    {
+        public static int[,] CreateTheGrid(int[,] seedData)
+        {
+            var rows = seedData.GetUpperBound(0) + 1;
+            var columns = seedData.GetUpperBound(1) + 1;
+            var grid = new int[rows, columns];
+
+            for (var row = 0; row < rows; row++)
+            {
+                for (var column = 0; column < columns; column++)
+                {
+                    grid[row, column] = seedData[row, column];
+                }
+            }
+            return grid;
+        }
+    }
+}
